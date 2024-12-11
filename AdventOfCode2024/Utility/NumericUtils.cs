663107156a34f6491644b948f57e0aa51dc111ff
Ryan Sweeney <sween119@hotmail.com>
2024-12-10 21:30:33 -0500

    clean up

--- AdventOfCode2024/Utility/NumericUtils.cs
+++ AdventOfCode2024/Utility/NumericUtils.cs
@@ -1,5 +1,4 @@
 using System.Numerics;
-using AdventOfCode2024.Enums;
 
 namespace AdventOfCode2024.Utility;
 
@@ -8,10 +7,5 @@ public static class NumericUtils
     public static Vector2 ToVector2(this Vector3 v3)
     {
         return new Vector2(v3.X, v3.Y);
-    }
-
-    public static string DisplayDirection(this Vector3 v3)
-    {
-        return $"<{v3.X}, {v3.Y}, {(Direction)(int)v3.Z}>";
     }
 }
