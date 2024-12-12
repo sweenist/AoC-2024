fd5b5112852421c304521b367d58545461b709ed
Ryan Sweeney <sween119@hotmail.com>
2024-12-11 22:42:57 -0500

    Day 11 tweaks with static definitions

--- AdventOfCode2024/Days/Day11.cs
+++ AdventOfCode2024/Days/Day11.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace AdventOfCode2024.Days;
 
 public class Day11 : IDay
@@ -43,30 +45,51 @@ public class Day11 : IDay
 
     private long Blink(long stone, int blinks)
     {
-        bool EvenDigits(long inVar, out long outVar)
+        static bool EvenDigits(long inVar, out long outVar)
         {
-            outVar = (long)Math.Floor(Math.Log10(inVar)) + 1;
-            return outVar % 2 == 0;
+            var value = EvenNumberLengths(inVar);
+            outVar = value / 2;
+            return value != 1;
         }
+
         if (blinks == 0)
             return 1L;
         var nextBlink = blinks - 1;
+
         if (_cache.TryGetValue(new StonedBlinks(stone, nextBlink), out var existing)) return existing;
-        long result;
-        switch (stone)
+        var result = stone switch
         {
-            case 0:
-                result = Blink(1L, nextBlink);
-                break;
-            case var x when EvenDigits(x, out var digits):
-                result = Blink(stone / (long)Math.Pow(10, digits), nextBlink)
-                + Blink(stone % (long)Math.Pow(10, digits), nextBlink);
-                break;
-            default:
-                result = Blink(stone * 2024, nextBlink);
-                break;
-        }
-        _cache[new StonedBlinks(stone, nextBlink)] = result;
+            0 => Blink(1L, nextBlink),
+            var x when EvenDigits(x, out var digits) => Blink(stone / TenPowers(digits), nextBlink)
+                                   + Blink(stone % TenPowers(digits), nextBlink),
+            _ => Blink(stone * 2024, nextBlink),
+        };
+
+        CollectionsMarshal.GetValueRefOrAddDefault(_cache, new StonedBlinks(stone, nextBlink), out bool _) = result;
         return result;
     }
+
+    private static long TenPowers(long power) => power switch
+    {
+        0 => 1,
+        1 => 10,
+        2 => 100,
+        3 => 1000,
+        4 => 10000,
+        5 => 100000,
+        6 => 1000000,
+        _ => (long)Math.Pow(10, power)
+    };
+
+    private static long EvenNumberLengths(long number) => number switch
+    {
+        >= 10 and <= 99 => 2,
+        >= 1000 and <= 9999 => 4,
+        >= 100000 and <= 999999 => 6,
+        >= 10000000 and <= 99999999 => 8,
+        >= 1000000000 and <= 9999999999 => 10,
+        >= 100000000000 and <= 999999999999 => 12,
+        >= 10000000000000 and <= 99999999999999 => 14,
+        _ => 1
+    };
 }
