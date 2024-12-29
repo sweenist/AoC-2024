bee8b62ae2326bc943fdaa58018b89f7fd528600
Ryan Sweeney <sween119@hotmail.com>
2024-12-28 22:53:48 -0500

    Day 16: finally on the right track

--- AdventOfCode2024/Days/Day16.cs
+++ AdventOfCode2024/Days/Day16.cs
@@ -8,39 +8,39 @@ namespace AdventOfCode2024.Days;
 
 public class Day16 : IDay
 {
-    //     private readonly string _example = @"###############
-    // #.......#....E#
-    // #.#.###.#.###.#
-    // #.....#.#...#.#
-    // #.###.#####.#.#
-    // #.#.#.......#.#
-    // #.#.#####.###.#
-    // #...........#.#
-    // ###.#.#####.#.#
-    // #...#.....#.#.#
-    // #.#.#.###.#.#.#
-    // #.....#...#.#.#
-    // #.###.#.#.#.#.#
-    // #S..#.....#...#
-    // ###############";
+    private readonly string _example = @"###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
 
-    private string _example = @"#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################";
+    //     private string _example = @"#################
+    // #...#...#...#..E#
+    // #.#.#.#.#.#.#.#.#
+    // #.#.#.#...#...#.#
+    // #.#.#.#.###.#.#.#
+    // #...#.#.#.....#.#
+    // #.#.#.#.#.#####.#
+    // #.#...#.#.#.....#
+    // #.#.#####.#.###.#
+    // #.#.#.......#...#
+    // #.#.###.#####.###
+    // #.#.#...#.....#.#
+    // #.#.#.#####.###.#
+    // #.#.#.........#.#
+    // #.#.#.#########.#
+    // #S#.............#
+    // #################";
 
     private readonly string[] _input;
     private readonly Maze _maze;
@@ -63,14 +63,14 @@ public class Day16 : IDay
 
     public void Part1()
     {
-        var result = _maze.Traverse().Min(t => t.PathScore);
+        var (result, _) = _maze.Traverse();
         Console.WriteLine($"Best reindeer track score is {result}");
     }
 
     public void Part2()
     {
-        var paths = _maze.Traverse();
-        var (path, bestScore) = paths.OrderBy(t => t.PathScore).First();
+        var (_, paths) = _maze.Traverse();
+        var result = paths.SelectMany(p => p).Distinct().ToList();
 
         var sb = new StringBuilder();
         for (var y = 0; y < _maze.Bounds.Height; y++)
@@ -78,13 +78,13 @@ public class Day16 : IDay
             for (var x = 0; x < _maze.Bounds.Width; x++)
             {
                 if (!_maze.Walkable[x, y]) sb.Append('#');
-                else if (path.Visited.Contains(new Point(x, y))) sb.Append('O');
+                else if (result.Contains(new Point(x, y))) sb.Append('O');
                 else sb.Append('.');
             }
             sb.Append('\n');
         }
         Console.WriteLine(sb);
-        Console.WriteLine($"The number of paths around the best seats are {path.Visited.Count}");
+        Console.WriteLine($"The number of paths around the best seats are {result.Count}");
     }
 
     private class Reindeer(Point Location, Vector Direction) : Actor(Location, Direction)
@@ -108,12 +108,14 @@ public class Day16 : IDay
                 {
                     var cell = input[y][x];
                     Walkable[x, y] = cell != '#';
-                    if (cell != '#') CumulativeScore.Add(new Point(x, y), int.MaxValue);
+                    if (cell != '#')
+                        foreach (var facing in Vector.CardinalPoints)
+                            CumulativeScore.Add((new Point(x, y), facing), int.MaxValue);
 
                     if (cell == startChar)
                     {
                         Start = new Point(x, y);
-                        CumulativeScore[Start] = 0;
+                        CumulativeScore[(Start, Vector.East)] = 0;
                     }
                     else if (cell == endChar)
                         End = new Point(x, y);
@@ -122,49 +124,56 @@ public class Day16 : IDay
 
         public Point Start { get; set; }
         public Point End { get; set; }
-        public Dictionary<Point, int> CumulativeScore { get; set; } = [];
+        public Dictionary<(Point, Vector), int> CumulativeScore { get; set; } = [];
 
-        public List<(Reindeer, int PathScore)> Traverse()
+        public (int BestScore, List<List<Point>> BestPaths) Traverse()
         {
-            var returnList = new List<(Reindeer, int PathScore)>();
+            var optimalPaths = new List<List<Point>>();
             var openList = new SortedSet<(int PathScore, Reindeer Reindeer)>(new PriorityComparer<Reindeer>())
                 {(0, new Reindeer(Start, Vector.East))};
+            var bestScore = int.MaxValue;
 
             while (openList.Count > 0)
             {
-                var point = openList.Min;
-                openList.Remove(point);
-                var (_, current) = point;
+                var composite = openList.Min;
+                openList.Remove(composite);
+                var (currentScore, current) = composite;
+                if (currentScore > bestScore) continue;
                 current.Visited.Add(current.Location);
 
                 var nextDirections = Vector.CardinalPoints.Where(x => x != current.Direction.Invert())
                     .Select(v => (Direction: v, NextLocation: current.Location + v))
                     .Where(vt => Walkable[vt.NextLocation.X, vt.NextLocation.Y]);
-                var hasBranches = nextDirections.Count() > 1;
 
                 foreach (var (direction, nextPosition) in nextDirections)
                 {
                     var weight = current.Direction != direction ? 1001 : 1;
-                    var visited = hasBranches ? [.. current.Visited] : current.Visited;
 
                     if (nextPosition == End)
                     {
-                        returnList.Add((new Reindeer(nextPosition, direction, [.. current.Visited]), point.PathScore + weight));
+                        composite.PathScore += weight;
+                        if (composite.PathScore < bestScore)
+                        {
+                            bestScore = composite.PathScore;
+                            optimalPaths = [[.. current.Visited]];
+                        }
+                        else if (composite.PathScore == bestScore)
+                            optimalPaths.Add(current.Visited);
+
+                        continue;
                     }
 
-                    if (!current.Visited.Contains(nextPosition)
-                        && Walkable[nextPosition.X, nextPosition.Y])
+                    var nextReindeer = new Reindeer(nextPosition, direction, [.. current.Visited]);
+                    var nextScore = composite.PathScore + weight;
+                    CumulativeScore.TryGetValue((nextPosition, direction), out var score);
+                    if (nextScore <= score)
                     {
-                        var nextReindeer = new Reindeer(nextPosition, direction, visited);
-                        var nodeScore = point.PathScore + weight;
-
-                        CumulativeScore.TryGetValue(nextPosition, out var score);
-                        CumulativeScore[nextPosition] = Math.Min(score, nodeScore);
-                        openList.Add((nodeScore, nextReindeer));
+                        CumulativeScore[(nextPosition, direction)] = nextScore;
+                        openList.Add((nextScore, nextReindeer));
                     }
                 }
             }
-            return returnList;
+            return (bestScore, optimalPaths);
         }
     }
 }
